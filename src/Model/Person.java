7364b43d1f842bb704d98aea90497c0832466347
MungC2010G2 <zhaiduong26z@gmail.com>
2021-01-16 12:15:17 +0700

    upgrade file test write and read file

--- src/Model/Person.java
+++ src/Model/Person.java
@@ -3,7 +3,7 @@ package Model;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public abstract class Person implements Salary, Comparable<Person> {
+public abstract class Person implements Salary, Comparable<Person>,Serializable {
     private String name;
     private LocalDate birthday;
 
---
+++ test/Storage/WriteAndReadFileTest.java
@@ -0,0 +1,34 @@
+package Storage;
+
+import Model.Person;
+import Model.Pharmacist;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WriteAndReadFileTest {
+    final static String FILE_NAME1 = "test.txt";
+    List<Person> personList = new ArrayList<>();
+    Pharmacist pharmacist = new Pharmacist("hanoi");
+
+    @Test
+    void writeFile() {
+        personList.add(pharmacist);
+        List<Person> expectedResult = personList;
+        WriteAndReadFile.writeFile(FILE_NAME1,personList);
+        List<Person> result = personList;
+        assertEquals(expectedResult,result);
+
+    }
+
+    @Test
+    void readFile() {
+        personList.add(pharmacist);
+        List<Person> expectedResult = personList;
+        List<Person> result = (List<Person>) WriteAndReadFile.readFile(FILE_NAME1);
+        assertEquals(expectedResult.toString(),result.toString());
+    }
+}
